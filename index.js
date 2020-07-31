420938926f53663d8a632f835459c986c8887ed2
Scott Semotan <65789616+scottsemotan@users.noreply.github.com>
2020-07-30 20:47:19 -0400

    updated index.html, css & js (#6)

--- index.js
+++ index.js
@@ -16,30 +16,31 @@ firebase.analytics();
 var provider = new firebase.auth.GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-function callGoogleSignIn(){
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-       // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-       // The signed-in user info.
-       var user = result.user;
-       document.location.href = 'moviesearch.html';
-       console.log(token);
-       // ...
- }).catch(function(error) {
-     // Handle Errors here.
-       var errorCode = error.code;
-       var errorMessage = error.message;
-       // The email of the user's account used.
-       var email = error.email;
-       // The firebase.auth.AuthCredential type that was used.
-       var credential = error.credential;
-       
-    // ...
- });
+  function callGoogleSignIn(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+         // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+         // The signed-in user info.
+         var user = result.user;
+         document.location.href = 'moviesearch.html';
+         console.log(token);
+         // ...
+   }).catch(function(error) {
+       // Handle Errors here.
+         var errorCode = error.code;
+         var errorMessage = error.message;
+         // The email of the user's account used.
+         var email = error.email;
+         // The firebase.auth.AuthCredential type that was used.
+         var credential = error.credential;
+         
+      // ...
+   });
 }
 ///Function to grab new user's signup info and grant access to search page.
 var signUp = document.getElementById('signUp');
+
 signUp.addEventListener('click', function(){
 var formName = document.getElementById('formName').value
 var formEmail = document.getElementById('formEmail').value
@@ -61,4 +62,5 @@ if(formName && formEmail && formPass) {
 }
 
 });
+
 
